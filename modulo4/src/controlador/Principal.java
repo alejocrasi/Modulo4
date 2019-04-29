8375702cbc6c3a175abeadd0ea66b63e2f306627
alejocrasi <alejocrasi@172.16.66.46>
2019-04-29 10:37:10 -0500

    prueba1

--- modulo4/src/controlador/Principal.java
+++ modulo4/src/controlador/Principal.java
@@ -1,5 +1,10 @@
 package controlador;
 
+import javax.swing.JOptionPane;
+
 public class Principal {
+	public static void main(String[] args) {
+		JOptionPane.showConfirmDialog(null, "si funciona");
+	}
 
 }
